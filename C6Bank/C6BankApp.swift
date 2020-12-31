90963ba2cc659fe8b9dc9f2c648bd53d4c501ca6
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-12-31 11:34:21 -0300

    Initial Commit

---
+++ C6Bank/C6BankApp.swift
@@ -0,0 +1,17 @@
+//
+//  C6BankApp.swift
+//  C6Bank
+//
+//  Created by Raphael Cerqueira on 31/12/20.
+//
+
+import SwiftUI
+
+@main
+struct C6BankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
